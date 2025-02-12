629eda78cef44a326401ee08b6b6349475a29640
devansh rathod <rathoddevansh53@gmail.com>
2025-02-12 16:34:07 +0530

    8th commit

--- beckend/Controllers/UserController.js
+++ beckend/Controllers/UserController.js
@@ -7,6 +7,8 @@ import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../Models/DoctorsModel.js";
 import appointmentModel from "../Models/AppointmentModel.js";
 import razorpay from "razorpay";
+import crypto from "crypto";
+
 // import orders from "razorpay/dist/types/orders.js";
 
 const RegisterUser = async (req, res) => {
